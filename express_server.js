4efa4917ed1bc3e27a5203ffb955d49a890310cb
Paige <paigekato@gmail.com>
2017-03-15 20:52:22 +0000

    adding changes up to UPDATING

--- express_server.js
+++ express_server.js
@@ -3,17 +3,17 @@ var app = express();
 var PORT = process.env.PORT || 8080; //default port 8080
 
 const bodyParser = require("body-parser");
-  app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({extended: true}));
 //allows us to access POST request parameters
 
 function generateRandomString() {
-  var r = " ";
+  var r = "";
   var s = "abcdefghijklmnopqrstuvwxyz0123456789";
   for(var i = 0; i < 6; i++) {
     r += s.charAt(Math.floor(Math.random() * s.length));
   }
   return r;
-};
+}
 
 
 app.set("view engine", "ejs");
@@ -36,31 +36,47 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });//info from files in view
 
+//for to retreive GET
 app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
 
-app.post("/urls", (req, res) => {  //corresponds with method=post
+//creating random string from new url in form POST
+app.post("/urls", (req, res) => {
   var newShortURL = generateRandomString();
-  if(urlDatabase === null) {
-    alert("Please Enter a Website");
-  } else {
   urlDatabase[newShortURL] = req.body.longURL; //if
-  res.redirect(`urls/${newShortURL}`);
-    };
+  res.redirect("urls");
 });
+//%{newShortURL}
 
+//redirecting short urls to longurls
 app.get("/u/:shortURL", (req, res) => {
+  if(!urlDatabase[req.params.shortURL]) {
+    res.status(400).redirect("https://http.cat/404") ;
+  } else {
   let longURL = urlDatabase[req.params.shortURL];
   res.redirect(longURL);
+  }
 });
 
 app.get("/urls/:id", (req, res) => {
-
   let templateVars = { shortURL: req.params.id, urls: urlDatabase };
   console.log(req.params.id);
-  res.render("urls_show", templateVars)
-  });
+  res.render("urls_show", templateVars); //get req RENDER/post redirects
+});
+//delete
+app.post("/urls/:id/delete", (req, res) => {
+  delete urlDatabase[req.params.id];
+  res.redirect("/urls");
+});
+
+//updating URLS once a submit UPDATE request should modify corresponding
+// longURLS and redirect back to /urls
+app.post("/urls/:id", (req, res) => {
+  urlDatabase[req.params.id] = req.body.longURL;
+  res.redirect("/urls/" + req.params.id);
+});
+
 
 
 
